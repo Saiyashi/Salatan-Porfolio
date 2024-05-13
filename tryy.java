216fd4ff730597dac6dea0af3e5683902c763b8b
Saiyashi <160900190+Saiyashi@users.noreply.github.com>
2024-05-13 13:19:21 +0800

    Add files via upload

---
+++ tryy.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+    
+public class tryy {
+
+    public static void main(String[] args) {
+        
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter Double");
+        Double You=input.nextDouble();
+        System.out.println("You Entered Double "+You);
+        System.out.println("Enter float");
+        Float youu=input.nextFloat();
+        System.out.println("You Entered float "+youu);
+        System.out.println("Enter number");
+        Double number=input.nextDouble();
+        System.out.println("Entered numbered "+number);
+        
+    
+    }
+}
